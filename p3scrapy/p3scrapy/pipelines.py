f731f5bbcbaf3c50fb757a46c9ce3012fce867f2
shanyue <a.shanyue@gmail.com>
2019-03-25 16:39:57 +0800

    update keywords

--- p3scrapy/p3scrapy/pipelines.py
+++ p3scrapy/p3scrapy/pipelines.py
@@ -4,15 +4,21 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import time
 
 
 class P3ScrapyPipeline(object):
     def process_item(self, item, spider):
         # fs = open(item['mdFilePath'], 'w')
+        date_time = item["date_time"]
+        if date_time:
+            pass
+        else:
+            date_time = time.strftime("%Y-%m-%d %H:%M", time.localtime())
         with open(item['mdFilePath'], 'w') as fs:
             fs.write('---\n')
             fs.write('title: ' + item["title"] + '\n')
-            fs.write('date: ' + item["date_time"] + '\n')
+            fs.write('date: ' + date_time + '\n')
             fs.write('keywords: ' + item["keywords"] + '\n')
             fs.write('description: ' + item["description"] + '\n')
             fs.write('categories: ' + item["categories"] + '\n')
