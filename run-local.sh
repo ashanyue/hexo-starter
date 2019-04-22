1296d121579cf5d5310aa08d5f8bb2816fad2ae3
shanyue <a.shanyue@gmail.com>
2019-04-22 11:38:48 +0800

    2019-04-22_11:38

---
+++ run-local.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+cd ./p2scrapy/
+pwd
+echo "=====start scrapy====="
+scrapy crawl dmoz
+echo "=====end scrapy====="
+cd ../
+pwd
+echo "=====start git====="
+datename=$(date +%Y-%m-%d_%H:%M)
+echo $datename
+git add ./source/*
+git commit -m "$datename"
+git push
