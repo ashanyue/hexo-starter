04a700e31a7c9262fe8c92aafe7158e1d312017b
shanyue <a.shanyue@gmail.com>
2019-04-22 10:30:14 +0800

    update keywords

---
+++ p2scrapy/p2scrapy/pipelines.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+class P2ScrapyPipeline(object):
+    def process_item(self, item, spider):
+        # fs = open(item['mdFilePath'], 'w')
+        date_time = item["date_time"]
+        if date_time:
+            pass
+        else:
+            date_time = time.strftime("%Y-%m-%d %H:%M", time.localtime())
+        with open(item['mdFilePath'], 'w') as fs:
+            fs.write('---\n')
+            fs.write('title: ' + item["title"] + '\n')
+            fs.write('date: ' + date_time + '\n')
+            fs.write('keywords: ' + item["keywords"] + '\n')
+            fs.write('description: ' + item["description"] + '\n')
+            fs.write('categories: ' + item["categories"] + '\n')
+            fs.write('---\n')
+            fs.write(item["content"] + '\n')
+            # fs.write('###### 转载自菲龙网')
+            return item
