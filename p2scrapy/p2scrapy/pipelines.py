58e3b8abcfc05e3a088caa385144320b3726c5e6
shanyue <a.shanyue@gmail.com>
2019-04-22 15:14:30 +0800

    add google analy

--- p2scrapy/p2scrapy/pipelines.py
+++ p2scrapy/p2scrapy/pipelines.py
@@ -4,27 +4,24 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import time
 
 class P2ScrapyPipeline(object):
     def process_item(self, item, spider):
         # fs = open(item['mdFilePath'], 'w')
-		#print('!@@@@@@@@@@@@@@@@@@@@')
-        date_time = item["date_timeq"]
+        date_time = item["date_time"]
         if date_time:
             pass
         else:
             date_time = time.strftime("%Y-%m-%d %H:%M", time.localtime())
         with open(item['mdFilePath'], 'w') as fs:
-			#print('!@@@@@@@@@@@@@@@@@@@@')
-            fs.write('---\n')
             fs.write('title: ' + item["title"] + '\n')
             fs.write('date: ' + date_time + '\n')
             fs.write('keywords: ' + item["keywords"] + '\n')
             fs.write('description: ' + item["description"] + '\n')
             fs.write('categories: ' + item["categories"] + '\n')
             fs.write('---\n')
-			fs.write(item["content"] + '\n')
+            fs.write(item["content"] + '\n')
             
             # fs.write('###### 转载自菲龙网')
             return item
